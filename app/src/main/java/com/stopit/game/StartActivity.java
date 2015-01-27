db9d1afacc35723688ea78ed1a90c64e92ce0d9b
shahdhr <shahdhr@ccs.neu.edu>
2015-01-27 15:03:52 -0500

    Home Screen

---
+++ app/src/main/java/com/stopit/game/StartActivity.java
@@ -0,0 +1,19 @@
+package com.stopit.game;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+
+public class StartActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+    }
+
+    public void onClickPlay(View view) {
+        Intent intent = new Intent(this,MainActivity.class);
+    }}
