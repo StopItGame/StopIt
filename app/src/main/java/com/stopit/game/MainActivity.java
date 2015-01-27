f34ff2ccbeb8bd7010434261ba8e97ac1e649217
Dharam Maniar <maniar.d@husky.neu.edu>
2015-01-27 18:03:34 -0500

    Changing to Image

--- app/src/main/java/com/stopit/game/MainActivity.java
+++ app/src/main/java/com/stopit/game/MainActivity.java
@@ -1,35 +1,16 @@
 package com.stopit.game;
 
 import android.app.Activity;
-import android.content.Context;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.util.Log;
-import android.util.TypedValue;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.BaseAdapter;
-import android.widget.GridView;
 import android.widget.ImageView;
 
 
 public class MainActivity extends Activity {
 
     private static final String TAG = "MainActivity";
-
-    private GridView gridViewThousand;
-    private GridView gridViewHundred;
-    private GridView gridViewTen;
-    private GridView gridViewUnit;
-    private LightAdapter lightAdapterThousand;
-    private LightAdapter lightAdapterHundred;
-    private LightAdapter lightAdapterTen;
-    private LightAdapter lightAdapterUnit;
-
-    private Boolean lightsThousand[];
-    private Boolean lightsHundred[];
-    private Boolean lightsTen[];
-    private Boolean lightsUnit[];
 
     private CountDownTimer timer;
     private int count;
@@ -39,36 +20,25 @@ public class MainActivity extends Activity {
     private int prevHundred = 0;
     private int prevThousand = 0;
 
+    private ImageView thousandImage;
+    private ImageView hundredImage;
+    private ImageView tenImage;
+    private ImageView unitImage;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        lightsThousand = new Boolean[91];
-        lightsHundred = new Boolean[91];
-        lightsTen = new Boolean[91];
-        lightsUnit = new Boolean[91];
-
-        gridViewUnit = (GridView) findViewById(R.id.activity_main_grid1);
-        gridViewTen = (GridView) findViewById(R.id.activity_main_grid2);
-        gridViewHundred = (GridView) findViewById(R.id.activity_main_grid3);
-        gridViewThousand = (GridView) findViewById(R.id.activity_main_grid4);
+        thousandImage = (ImageView) findViewById(R.id.activity_main_image_thousand);
+        hundredImage = (ImageView) findViewById(R.id.activity_main_image_hundred);
+        tenImage = (ImageView) findViewById(R.id.activity_main_image_ten);
+        unitImage = (ImageView) findViewById(R.id.activity_main_image_unit);
 
         count = 0;
         updateLights(count);
 
-        lightAdapterThousand = new LightAdapter(getApplicationContext(), lightsThousand);
-        lightAdapterHundred = new LightAdapter(getApplicationContext(), lightsHundred);
-        lightAdapterTen = new LightAdapter(getApplicationContext(), lightsTen);
-        lightAdapterUnit = new LightAdapter(getApplicationContext(), lightsUnit);
-
-        gridViewThousand.setAdapter(lightAdapterThousand);
-        gridViewHundred.setAdapter(lightAdapterHundred);
-        gridViewTen.setAdapter(lightAdapterTen);
-        gridViewUnit.setAdapter(lightAdapterUnit);
-
-
-        timer = new CountDownTimer(60 * 1000, 50) {
+        timer = new CountDownTimer(60 * 1000, 1) {
             @Override
             public void onTick(long millisUntilFinished) {
                 Log.d(TAG, "Count = " + count);
@@ -96,159 +66,48 @@ public class MainActivity extends Activity {
         int thousand = (count / 1000);
 
         if ((unit != prevUnit) || (count == 0)) {
-            updateLightsArray(unit, 1);
-            lightAdapterUnit = new LightAdapter(getApplicationContext(), lightsUnit);
-            gridViewUnit.setAdapter(lightAdapterUnit);
+            unitImage.setImageDrawable(getDrawableImage(unit));
         }
         if ((ten != prevTen) || (count == 0)) {
-            updateLightsArray(ten, 2);
-            lightAdapterTen = new LightAdapter(getApplicationContext(), lightsTen);
-            gridViewTen.setAdapter(lightAdapterTen);
+            tenImage.setImageDrawable(getDrawableImage(ten));
         }
         if ((hundred != prevHundred) || (count == 0)) {
-            updateLightsArray(hundred, 3);
-            lightAdapterHundred = new LightAdapter(getApplicationContext(), lightsHundred);
-            gridViewHundred.setAdapter(lightAdapterHundred);
+            hundredImage.setImageDrawable(getDrawableImage(hundred));
         }
         if ((thousand != prevThousand) || (count == 0)) {
-            updateLightsArray(thousand, 4);
-            lightAdapterThousand = new LightAdapter(getApplicationContext(), lightsThousand);
-            gridViewThousand.setAdapter(lightAdapterThousand);
+            thousandImage.setImageDrawable(getDrawableImage(thousand));
         }
-
         prevThousand = thousand;
         prevHundred = hundred;
         prevTen = ten;
         prevUnit = unit;
     }
 
-    private void updateLightsArray(int unit, int position) {
-        for (int i = 0; i < 91; i++) {
-            if (position == 1) {
-                lightsUnit[i] = false;
-            } else if (position == 2) {
-                lightsTen[i] = false;
-            } else if (position == 3) {
-                lightsHundred[i] = false;
-            } else if (position == 4) {
-                lightsThousand[i] = false;
-            }
-        }
-
-        for (int i = 0; i < 91; i++) {
-            boolean needToChange = false;
-            switch (unit) {
-                case 0:
-                    if ((i == 1) || (i == 2) || (i == 3) || (i == 4) || (i == 5) || (i == 8) || (i == 12) || (i == 15) || (i == 19) || (i == 22) || (i == 26) || (i == 29) || (i == 33) || (i == 36) || (i == 40) || (i == 43) || (i == 47) || (i == 50) || (i == 54) || (i == 57) || (i == 61) || (i == 64) || (i == 68) || (i == 71) || (i == 75) || (i == 78) || (i == 82) || (i == 85) || (i == 86) || (i == 87) || (i == 88) || (i == 89)) {
-                        needToChange = true;
-                    }
-                    break;
-                case 1:
-                    if ((i == 3) || (i == 10) || (i == 17) || (i == 24) || (i == 31) || (i == 38) || (i == 45) || (i == 52) || (i == 59) || (i == 66) || (i == 73) || (i == 80) || (i == 87)) {
-                        needToChange = true;
-                    }
-                    break;
-                case 2:
-                    if ((i == 1) || (i == 2) || (i == 3) || (i == 4) || (i == 5) || (i == 12) || (i == 19) || (i == 26) || (i == 33) || (i == 40) || (i == 43) || (i == 44) || (i == 45) || (i == 46) || (i == 47) || (i == 50) || (i == 57) || (i == 64) || (i == 71) || (i == 78) || (i == 85) || (i == 86) || (i == 87) || (i == 88) || (i == 89)) {
-                        needToChange = true;
-                    }
-                    break;
-                case 3:
-                    if ((i == 1) || (i == 2) || (i == 3) || (i == 4) || (i == 5) || (i == 12) || (i == 19) || (i == 26) || (i == 33) || (i == 40) || (i == 43) || (i == 44) || (i == 45) || (i == 46) || (i == 47) || (i == 54) || (i == 61) || (i == 68) || (i == 75) || (i == 82) || (i == 85) || (i == 86) || (i == 87) || (i == 88) || (i == 89)) {
-                        needToChange = true;
-                    }
-                    break;
-                case 4:
-                    if ((i == 1) || (i == 5) || (i == 8) || (i == 12) || (i == 15) || (i == 19) || (i == 22) || (i == 26) || (i == 29) || (i == 33) || (i == 36) || (i == 40) || (i == 43) || (i == 44) || (i == 45) || (i == 46) || (i == 47) || (i == 54) || (i == 61) || (i == 68) || (i == 75) || (i == 82) || (i == 89)) {
-                        needToChange = true;
-                    }
-                    break;
-                case 5:
-                    if ((i == 1) || (i == 2) || (i == 3) || (i == 4) || (i == 5) || (i == 8) || (i == 15) || (i == 22) || (i == 29) || (i == 36) || (i == 43) || (i == 44) || (i == 45) || (i == 46) || (i == 47) || (i == 54) || (i == 61) || (i == 68) || (i == 75) || (i == 82) || (i == 85) || (i == 86) || (i == 87) || (i == 88) || (i == 89)) {
-                        needToChange = true;
-                    }
-                    break;
-                case 6:
-                    if ((i == 1) || (i == 2) || (i == 3) || (i == 4) || (i == 5) || (i == 8) || (i == 15) || (i == 22) || (i == 29) || (i == 36) || (i == 43) || (i == 44) || (i == 45) || (i == 46) || (i == 47) || (i == 50) || (i == 54) || (i == 57) || (i == 61) || (i == 64) || (i == 68) || (i == 71) || (i == 75) || (i == 78) || (i == 82) || (i == 85) || (i == 86) || (i == 87) || (i == 88) || (i == 89)) {
-                        needToChange = true;
-                    }
-                    break;
-                case 7:
-                    if ((i == 1) || (i == 2) || (i == 3) || (i == 4) || (i == 5) || (i == 12) || (i == 19) || (i == 26) || (i == 33) || (i == 40) || (i == 47) || (i == 54) || (i == 61) || (i == 68) || (i == 75) || (i == 82) || (i == 89)) {
-                        needToChange = true;
-                    }
-                    break;
-                case 8:
-                    if ((i == 1) || (i == 2) || (i == 3) || (i == 4) || (i == 5) || (i == 8) || (i == 12) || (i == 15) || (i == 19) || (i == 22) || (i == 26) || (i == 29) || (i == 33) || (i == 36) || (i == 40) || (i == 43) || (i == 44) || (i == 45) || (i == 46) || (i == 47) || (i == 50) || (i == 54) || (i == 57) || (i == 61) || (i == 64) || (i == 68) || (i == 71) || (i == 75) || (i == 78) || (i == 82) || (i == 85) || (i == 86) || (i == 87) || (i == 88) || (i == 89)) {
-                        needToChange = true;
-                    }
-                    break;
-                case 9:
-                    if ((i == 1) || (i == 2) || (i == 3) || (i == 4) || (i == 5) || (i == 8) || (i == 12) || (i == 15) || (i == 19) || (i == 22) || (i == 26) || (i == 29) || (i == 33) || (i == 36) || (i == 40) || (i == 43) || (i == 44) || (i == 45) || (i == 46) || (i == 47) || (i == 54) || (i == 61) || (i == 68) || (i == 75) || (i == 82) || (i == 85) || (i == 86) || (i == 87) || (i == 88) || (i == 89)) {
-                        needToChange = true;
-                    }
-                    break;
-                default:
-                    break;
-            }
-            if (needToChange) {
-                if (position == 1) {
-                    lightsUnit[i] = true;
-                } else if (position == 2) {
-                    lightsTen[i] = true;
-                } else if (position == 3) {
-                    lightsHundred[i] = true;
-                } else if (position == 4) {
-                    lightsThousand[i] = true;
-                }
-            }
+    private Drawable getDrawableImage(int number) {
+        switch (number) {
+            case 0:
+                return getResources().getDrawable(R.drawable.zero);
+            case 1:
+                return getResources().getDrawable(R.drawable.one);
+            case 2:
+                return getResources().getDrawable(R.drawable.two);
+            case 3:
+                return getResources().getDrawable(R.drawable.three);
+            case 4:
+                return getResources().getDrawable(R.drawable.four);
+            case 5:
+                return getResources().getDrawable(R.drawable.five);
+            case 6:
+                return getResources().getDrawable(R.drawable.six);
+            case 7:
+                return getResources().getDrawable(R.drawable.seven);
+            case 8:
+                return getResources().getDrawable(R.drawable.eight);
+            case 9:
+                return getResources().getDrawable(R.drawable.nine);
+            default:
+                return null;
         }
     }
 
-    public class LightAdapter extends BaseAdapter {
-
-        private Context context;
-        private Boolean lights[];
-
-        public LightAdapter(Context context, Boolean lights[]) {
-            this.context = context;
-            this.lights = lights;
-        }
-
-        @Override
-        public int getCount() {
-            return lights.length;
-        }
-
-        @Override
-        public Object getItem(int position) {
-            return null;
-        }
-
-        @Override
-        public long getItemId(int position) {
-            return position;
-        }
-
-        @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
-            ImageView light;
-            if (convertView == null) {
-                light = new ImageView(context);
-                int height = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 10,
-                        getResources().getDisplayMetrics());
-                int width = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 10,
-                        getResources().getDisplayMetrics());
-                light.setLayoutParams(new GridView.LayoutParams(height, width));
-            } else {
-                light = (ImageView) convertView;
-            }
-            if (lights[position]) {
-                light.setImageDrawable(context.getResources().getDrawable(R.drawable.on_light));
-            } else {
-                light.setImageDrawable(context.getResources().getDrawable(R.drawable.off_light));
-            }
-            return light;
-        }
-    }
 }
