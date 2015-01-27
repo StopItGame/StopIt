321e49583df2485a3911fd7a370e0e68f3b47928
Dharam Maniar <maniar.d@husky.neu.edu>
2015-01-27 15:10:27 -0500

    Integrate start activity

--- app/src/main/java/com/stopit/game/StartActivity.java
+++ app/src/main/java/com/stopit/game/StartActivity.java
@@ -4,16 +4,31 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 
-public class StartActivity extends Activity {
+public class StartActivity extends Activity implements View.OnClickListener {
+
+    private Button play;
+    private Button quit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+        play = (Button) findViewById(R.id.activity_main_button_play);
+        play.setOnClickListener(this);
+        quit = (Button) findViewById(R.id.activity_main_button_quit);
+        quit.setOnClickListener(this);
     }
 
-    public void onClickPlay(View view) {
-        Intent intent = new Intent(this,MainActivity.class);
-    }}
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == play.getId()) {
+            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+        }
+        if (v.getId() == quit.getId()) {
+            finish();
+        }
+    }
+}
