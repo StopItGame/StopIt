973c72675606521e28e5e86951e4eb465ac17c0e
Dharam Maniar <maniar.d@husky.neu.edu>
2015-01-27 22:01:03 -0500

    Change to text mode

--- app/src/main/java/com/stopit/game/MainActivity.java
+++ app/src/main/java/com/stopit/game/MainActivity.java
@@ -1,11 +1,11 @@
 package com.stopit.game;
 
 import android.app.Activity;
-import android.graphics.drawable.Drawable;
+import android.content.Context;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.os.CountDownTimer;
-import android.util.Log;
-import android.widget.ImageView;
+import android.widget.TextView;
 
 
 public class MainActivity extends Activity {
@@ -20,32 +20,44 @@ public class MainActivity extends Activity {
     private int prevHundred = 0;
     private int prevThousand = 0;
 
-    private ImageView thousandImage;
-    private ImageView hundredImage;
-    private ImageView tenImage;
-    private ImageView unitImage;
+    private TextView countTextThousand;
+    private TextView countTextHundred;
+    private TextView countTextTen;
+    private TextView countTextUnit;
+
+    private Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        thousandImage = (ImageView) findViewById(R.id.activity_main_image_thousand);
-        hundredImage = (ImageView) findViewById(R.id.activity_main_image_hundred);
-        tenImage = (ImageView) findViewById(R.id.activity_main_image_ten);
-        unitImage = (ImageView) findViewById(R.id.activity_main_image_unit);
-
-        count = 0;
-        updateLights(count);
-
-        timer = new CountDownTimer(60 * 1000, 1) {
+        context = this;
+        countTextThousand = (TextView) findViewById(R.id.activity_main_textview_thousand);
+        countTextHundred = (TextView) findViewById(R.id.activity_main_textview_hundred);
+        countTextTen = (TextView) findViewById(R.id.activity_main_textview_ten);
+        countTextUnit = (TextView) findViewById(R.id.activity_main_textview_unit);
+        Typeface tf = Typeface.createFromAsset(getAssets(), "digital.ttf");
+        countTextThousand.setTypeface(tf);
+        countTextHundred.setTypeface(tf);
+        countTextTen.setTypeface(tf);
+        countTextUnit.setTypeface(tf);
+        countTextThousand.setText("0");
+        countTextHundred.setText("0");
+        countTextTen.setText("0");
+        countTextUnit.setText("0");
+        timer = new CountDownTimer(60 * 1000, 50) {
             @Override
             public void onTick(long millisUntilFinished) {
-                Log.d(TAG, "Count = " + count);
                 count++;
                 if (count < 10000) {
-                    updateLights(count);
-                    Log.d(TAG, "Updated lights for count = " + count);
+                    int unit = count % 10;
+                    int ten = (count / 10) % 10;
+                    int hundred = (count / 100) % 10;
+                    int thousand = (count / 1000);
+                    countTextThousand.setText(thousand + "");
+                    countTextHundred.setText(hundred + "");
+                    countTextTen.setText(ten + "");
+                    countTextUnit.setText(unit + "");
                 }
             }
 
@@ -56,58 +68,6 @@ public class MainActivity extends Activity {
         };
         timer.start();
 
-    }
-
-    private void updateLights(int count) {
-
-        int unit = count % 10;
-        int ten = (count / 10) % 10;
-        int hundred = (count / 100) % 10;
-        int thousand = (count / 1000);
-
-        if ((unit != prevUnit) || (count == 0)) {
-            unitImage.setImageDrawable(getDrawableImage(unit));
-        }
-        if ((ten != prevTen) || (count == 0)) {
-            tenImage.setImageDrawable(getDrawableImage(ten));
-        }
-        if ((hundred != prevHundred) || (count == 0)) {
-            hundredImage.setImageDrawable(getDrawableImage(hundred));
-        }
-        if ((thousand != prevThousand) || (count == 0)) {
-            thousandImage.setImageDrawable(getDrawableImage(thousand));
-        }
-        prevThousand = thousand;
-        prevHundred = hundred;
-        prevTen = ten;
-        prevUnit = unit;
-    }
-
-    private Drawable getDrawableImage(int number) {
-        switch (number) {
-            case 0:
-                return getResources().getDrawable(R.drawable.zero);
-            case 1:
-                return getResources().getDrawable(R.drawable.one);
-            case 2:
-                return getResources().getDrawable(R.drawable.two);
-            case 3:
-                return getResources().getDrawable(R.drawable.three);
-            case 4:
-                return getResources().getDrawable(R.drawable.four);
-            case 5:
-                return getResources().getDrawable(R.drawable.five);
-            case 6:
-                return getResources().getDrawable(R.drawable.six);
-            case 7:
-                return getResources().getDrawable(R.drawable.seven);
-            case 8:
-                return getResources().getDrawable(R.drawable.eight);
-            case 9:
-                return getResources().getDrawable(R.drawable.nine);
-            default:
-                return null;
-        }
     }
 
 }
